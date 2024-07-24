36e81bcdf0373df974bb1bd911f583a8058c4a87
Gabriel Napolis <gabrielnapolis1604@gmail.com>
2024-07-23 22:57:13 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PropertiesModule } from './properties/properties.module';
 import { UsersModule } from './users/users.module';
-import { AmenitiesModule } from './amenities/amenities.module';
 import { DatabaseModule } from './database/database.module';
 
 
 @Module({
-  imports: [PropertiesModule, UsersModule, AmenitiesModule,DatabaseModule],
+  imports: [PropertiesModule, UsersModule,DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
