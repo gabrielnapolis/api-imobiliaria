681edf7db9a90929add628abb55a68677292f7e3
David Szturm <szturm.david@gmail.com>
2024-07-31 20:43:09 -0300

    save

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from '../database/database.module';
 import { userProviders } from './users.providers';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, PassportModule, JwtModule.register({
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '60s' },
+  }),],
+
   controllers: [UsersController],
   providers: [...userProviders,UsersService],
 })
