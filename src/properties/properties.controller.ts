5200765a7c2fe27736edade2aedad0bc24a282f7
David Szturm <szturm.david@gmail.com>
2024-08-22 13:49:03 -0300

    image_upload

--- src/properties/properties.controller.ts
+++ src/properties/properties.controller.ts
@@ -4,6 +4,8 @@ import { CreatePropertyDto } from './dto/create-property.dto';
 import { FindPropertyDto } from './dto/find-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
 import { FileInterceptor } from '@nestjs/platform-express/multer';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 
 @Controller('properties')
@@ -41,9 +43,19 @@ export class PropertiesController {
   }
 
   @Post("/image/:id")
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads'
+      , filename: (req, file, cb) => {
+        // Generating a 32 random chars long string
+        const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
+        //Calling the callback passing the random name generated with the original extension name
+        cb(null, `${randomName}${extname(file.originalname)}`)
+      }
+    })
+  }))
   uploadFile(@UploadedFile() file: Express.Multer.File,@Param('id') id: number) {
-    this.propertiesService.savePhoto(id,file.path)
+    this.propertiesService.savePhoto(id,file.filename)
     console.log(file);
   }
   @Delete('/photo/:id')
